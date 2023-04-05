79bdbb9b03eddb05197ccede6837e26c4ba17109
Evana Perveen Eva <76217577+evana27perveen@users.noreply.github.com>
2023-04-05 20:04:45 +0600

    Update admin.py

--- App_main/admin.py
+++ App_main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from App_main.models import *
 
 # Register your models here.
+admin.site.register(Company)
+admin.site.register(Employee)
+admin.site.register(Device)
+admin.site.register(DeviceLog)
